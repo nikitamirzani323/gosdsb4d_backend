3e435c45f92d17510f0d8b43ee83203e09d26e89
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-11-02 19:40:28 +0700

    New Module Admin Rule

--- sveltemdb/src/App.svelte
+++ sveltemdb/src/App.svelte
@@ -4,12 +4,13 @@
 	import Navbar from "./components/Navbar.svelte";
 	import Dashboard from "./pages/dashboard/Dashboard.svelte";
 	import Admin from "./pages/admin/Admin.svelte";
+	import Adminrule from "./pages/adminrule/Adminrule.svelte";
 	import Sdsb4dday from "./pages/sdsb4dday/Sdsb4dday.svelte";
 	import Sdsb4dnight from "./pages/sdsb4dnight/Sdsb4dnight.svelte";
 	import Login from "./pages/Login.svelte";
-	import NotFound from  "./pages/NotFound.svelte"
-	export let table_header_font
-	export let table_body_font
+	import NotFound from "./pages/NotFound.svelte";
+	export let table_header_font;
+	export let table_body_font;
 	let token = localStorage.getItem("token");
 	let routes = "";
 	let isNav = false;
@@ -24,32 +25,39 @@
 		isNav = true;
 		routes = {
 			"/": wrap({
-				props:{
-					table_header_font:table_header_font,
-					table_body_font:table_body_font
+				props: {
+					table_header_font: table_header_font,
+					table_body_font: table_body_font,
 				},
 				component: Dashboard,
 			}),
 			"/sdsb4dday": wrap({
 				component: Sdsb4dday,
-				props:{
-					table_header_font:table_header_font,
-					table_body_font:table_body_font
-				}
+				props: {
+					table_header_font: table_header_font,
+					table_body_font: table_body_font,
+				},
 			}),
 			"/sdsb4dnight": wrap({
 				component: Sdsb4dnight,
-				props:{
-					table_header_font:table_header_font,
-					table_body_font:table_body_font
-				}
+				props: {
+					table_header_font: table_header_font,
+					table_body_font: table_body_font,
+				},
 			}),
 			"/admin": wrap({
 				component: Admin,
-				props:{
-					table_header_font:table_header_font,
-					table_body_font:table_body_font
-				}
+				props: {
+					table_header_font: table_header_font,
+					table_body_font: table_body_font,
+				},
+			}),
+			"/adminrule": wrap({
+				component: Adminrule,
+				props: {
+					table_header_font: table_header_font,
+					table_body_font: table_body_font,
+				},
 			}),
 			"*": NotFound,
 		};
